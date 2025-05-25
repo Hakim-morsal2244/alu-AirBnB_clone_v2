406ab6e736d70c6eb67a28a04607399424ccde10
Morsal Hakim <m.hakim@alustudent.com>
2025-05-26 01:20:35 +0430

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
+""" State Module for HBNB project """
 import os
 
 from models.base_model import BaseModel, Base
@@ -10,9 +10,6 @@ from sqlalchemy.orm import relationship
 class Amenity(BaseModel, Base):
     """Amenities of a place"""
     __tablename__ = 'amenities'
-
     name = Column(String(128), nullable=False)
-
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        place_amenities = relationship('Place', secondary="place_amenity",
-                                       back_populates="amenities")
+        place_amenities = relationship('Place', secondary="place_amenity")
