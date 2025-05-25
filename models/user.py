7efaf45433a075b3e0b547a2ab2aa0155fc24184
Morsal Hakim <m.hakim@alustudent.com>
2025-05-26 01:24:28 +0430

    Update user.py

--- models/user.py
+++ models/user.py
@@ -4,18 +4,26 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
 
+
 class User(BaseModel, Base):
-    """This is the class for user"""
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
     __tablename__ = 'users'
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-
-    places = relationship("Place", backref="user", cascade="all, delete-orphan")
-    reviews = relationship("Review", backref="user", cascade="all, delete-orphan")
-
-# IMPORT AFTER CLASS TO AVOID CIRCULAR IMPORT
-from models.place import Place
-from models.review import Review
+    places = relationship(
+        'Place',
+        backref='user',
+        cascade='all, delete-orphan')
+    reviews = relationship(
+        'Review',
+        backref='user',
+        cascade='all, delete-orphan')
