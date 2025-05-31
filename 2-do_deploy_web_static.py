9bf0b1aa80f1fcdb6b2d84e5266b294f85bb7ab2
Morsal Hakim <m.hakim@alustudent.com>
2025-05-31 11:40:33 -0700

    Fix deployment script to follow PEP8 and correct symlink

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,17 +1,20 @@
-# -*- coding: utf-8 -*-
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 from fabric.api import env, run, put
 import os
 
-# ✅ Correct IPs of your web-01 and web-02 servers
+# Correct IPs of your web-01 and web-02 servers
 env.hosts = ['3.86.189.117', '54.236.227.49']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'  # Make sure this is your private key path
+
 
 def do_deploy(archive_path):
     """Distributes an archive to the web servers."""
     if not os.path.exists(archive_path):
         return False
+
     try:
         file_name = archive_path.split("/")[-1]
         folder_name = "/data/web_static/releases/" + file_name.split(".")[0]
@@ -25,7 +28,7 @@ def do_deploy(archive_path):
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(folder_name))
 
-        # ✅ Create symlink so that /hbnb_static points to the deployed content
+        # Create symlink so that /hbnb_static points to the deployed content
         run("sudo ln -sf /data/web_static/current /var/www/html/hbnb_static")
 
         print("New version deployed!")
